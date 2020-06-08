1c065c747bc0779c92ffbef16e7a7882d130f65e
André Santos <andrefs@andrefs.com>
2020-06-08 13:17:41 +0100

    Add download status bar

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-const CETEMPublico        = require('./lib/cetem-publico');
-const Token               = require('./lib/token');
-const Sentence            = require('./lib/sentence');
-const Paragraph           = require('./lib/paragraph');
-const MultiWordExpression = require('./lib/mwe');
-const Title               = require('./lib/title');
-const Authors             = require('./lib/authors');
-const Extract             = require('./lib/extract');
+const CETEMPublico = require('./lib/cetem-publico');
+const Title        = require('./lib/title');
+const Authors      = require('./lib/authors');
+const Extract      = require('./lib/extract');
 
 module.exports = {
   CETEMPublico,
-  Token,
-  Sentence,
-  Paragraph,
-  MultiWordExpression,
   Title,
   Authors,
   Extract
